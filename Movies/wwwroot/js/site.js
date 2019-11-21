2f2dabba09656085b451d99a9c4528f1d7d54737
Marissa Shivers <mxs@ksu.edu>
2019-11-21 14:35:20 -0600

    branch commits

--- Movies/wwwroot/js/site.js
+++ Movies/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var movieEntries = document.getElementsByClassName("movie-entry");
+
+var form = document.getElementById("search-and-filter-form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var i, entry;
+
+    var term = document.getElementById("search").value;
+    if (term) {
+        for (i = 0; i < movieEntries.length; i++) {
+            entry = movieEntries[i];
+            if (entry.dataList.title.includes(term)) {
+                entry.style.display = 'block';
+            }
+            else {
+                entry.style.display = 'none';
+            }
+        }
+    }
+});
